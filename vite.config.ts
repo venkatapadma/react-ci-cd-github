3cc92e39a4c39eb7790d92c5c575f1fd888c2e5c
venkatapadma <padmaganapathi568@gmail.com>
2026-05-07 12:01:49 +0100

    ci/cd

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/react-ci-cd-github/',
+  server: {
+    port: 3001,
+    open: true,
+  },
 })
